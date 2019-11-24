e5f9c98508046337bfc5d837c5075336ddb78878
Cho-HyeJin <hejin222@naver.com>
2019-11-24 23:54:46 +0900

    servo 1.5
    if (Owner):  not block, waiting
    else: blocking

--- servo test.py
+++ servo test.py
@@ -1,23 +1,34 @@
 import RPi.GPIO as GPIO
 import time
+from random import randint
 
 pin =18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
 p= GPIO.PWM(pin, 50)
 p.start(0)
-cnt = 0
 
-try:
-    while True:
-        p.ChangeDutyCycle(12.5) #최댓값
-        time.sleep(1)
-        p.ChangeDutyCycle(7.5) #0
-        time.sleep(1)
-        p.ChangeDutyCycle(2.5) #최솟값
-        time.sleep(1)
+isOwner = 3
 
-except KeybordInterrupt:
-     p.stop()
+
+for i in range(3):
+    #initial point
+    p.ChangeDutyCycle(12.5)
+    time.sleep(3)
+        
+    isOwner = randint(0,1)
+    print(i, '-> ', isOwner, '\n')
     
-GPIO.cleanup()
+    if(isOwner == 0):
+        print("no")
+        p.ChangeDutyCycle(12.5)
+        time.sleep(2)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(2)
+        p.ChangeDutyCycle(2.5)
+        time.sleep(3)
+    else:
+    print("owner!")
+    time.sleep(3)
+
+
